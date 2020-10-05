00917f4b0a282291c1c073aff0cda25385d9f2f0
Nwafor Chukwunonso <30531822+stepchuks9@users.noreply.github.com>
2020-10-05 12:22:51 +0100

    Create path.cpp

---
+++ path.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <functional>
+#include <string>
+#include <vector>
+
+#define BidirectionalIterator typename
+
+/*
+template <BidirectionalIterator I>
+inline
+I reverse_find(I f, I l, const decltype(*I())& val) {
+    if (f != l) while(f != --l && *l != val);
+    return l;
+}
+
+inline 
+std::pair<std::string, std::string>
+Split(const std::string& s) {
+    auto i = reverse_find(s.begin(), s.end(), '/');
+    return { std::string(s.begin(), i+1),
+                std::string(i+1, s.end())};
+}
+
+*/
+
+template <BidirectionalIterator I>
+inline 
+I reverse_find_unguarded(I f, I l, const decltype(*I())& val) {
+    while (f != --l && *l != val); 
+    return l;
+}
+
+template <BidirectionalIterator I>
+inline 
+I reverse_find(I f, I l, const decltype(*I())& val) {
+    return f != l ? reverse_find_unguarded(f,l,val) 
+            : l;
+}
+
+/*
+inline 
+std::pair<std::string, std::string>
+Split(const std::string& s) {
+    auto i = reverse_find(s.begin(), s.end(), '/');
+    return {std::string(s.begin(), i+1),
+                std::string(i+1, s.end())};
+}
+*/
+/*
+inline 
+std::pair<std::string, std::string>
+Split(const std::string& s) {
+    auto i = s.begin() != s.end() ?
+        reverse_find_unguarded(s.begin(), s.end(), '/')+1
+            : s.end();
+    return {std::string(s.begin(), i),
+                std::string(i, s.end())};
+}
+*/
+
+
+inline 
+std::pair<std::string, std::string>
+Split(const std::string& s) {
+    auto i = s.begin() == s.end() ? s.end() 
+        : reverse_find_unguarded(s.begin(), s.end(), '/');
+    i =  s.begin() == i ? i : i+1;        
+    return {std::string(s.begin(), i),
+                std::string(i, s.end())};
+}
+
+
+
+
+int main() {
+    std::vector<std::string> vec {
+        "static/myfile.css",
+        "myfile.css",
+        ""};
+
+    for(auto s : vec) {
+        auto [x, y] = Split(s);
+        std::cout << "dir: " << x << ", " << "file: " << y << '\n';
+    }
+    
+}
